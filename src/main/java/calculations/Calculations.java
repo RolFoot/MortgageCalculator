f6c6c379842dbc53d14f849afebcfe2fe97904c0
Roland <rolxxxx2@gmail.com>
2023-04-05 14:06:18 +0300

    linear formula working

--- src/main/java/calculations/Calculations.java
+++ src/main/java/calculations/Calculations.java
@@ -16,22 +16,37 @@ public class Calculations {
     }
 
     public double calculateMonthlyPaymentAnnuity(Mortgage mortgage) {
-        double monthlyPayment = 0;
+        double monthlyPayment;
 
         double interest = mortgage.getInterest() / 100 / MONTHS_IN_YEAR;
         double value = mortgage.getValue() - mortgage.getDownPayment();
-
         double term = (double) mortgage.getLoanTerm() * MONTHS_IN_YEAR;
 
         monthlyPayment = value * (interest * Math.pow(1 + interest, term)) / (Math.pow(1 + interest, term) - 1);
-
 
 
         return monthlyPayment;
     }
 
     public double calculateMonthlyPaymentLinear(Mortgage mortgage) {
-        double monthlyPayment = 1;
+        double monthlyPayment;
+
+        double interest = mortgage.getInterest() / 100 / MONTHS_IN_YEAR;
+        double value = mortgage.getValue() - mortgage.getDownPayment();
+        double term = (double) mortgage.getLoanTerm() * MONTHS_IN_YEAR;
+
+        double currentMonth = 1;
+
+        // P = (L / n) + (L - (m - 1) x (L / n)) x i
+        // Where:
+        //P = the monthly payment
+        //L = the loan amount
+        //n = the total number of months in the loan term
+        //m = the number of the current month
+        //i = the interest rate per month (annual interest rate divided by 12)
+
+        monthlyPayment = (value / term) + (value - (currentMonth - 1) * (value / term)) * interest;
+
         return monthlyPayment;
     }
 }
