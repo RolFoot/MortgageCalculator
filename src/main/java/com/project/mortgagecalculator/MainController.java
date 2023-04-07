e8d9f58feb06be058072e0f56819360a24f6283c
Roland <rolxxxx2@gmail.com>
2023-04-07 08:19:41 +0300

    Added graph viewing functionality

--- src/main/java/com/project/mortgagecalculator/MainController.java
+++ src/main/java/com/project/mortgagecalculator/MainController.java
@@ -3,13 +3,19 @@ package com.project.mortgagecalculator;
 import calculations.Calculations;
 import calculations.ExcelData;
 import calculations.Mortgage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Alert;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import org.apache.commons.math3.util.Precision;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainController {
@@ -48,7 +54,7 @@ public class MainController {
     private TextField delayInterestTxt;
 
     @FXML
-    private LineChart chart;
+    private LineChart<Integer,Number> chart;
 
     Mortgage mortgage = new Mortgage();
 
@@ -104,36 +110,12 @@ public class MainController {
         getInfoLoan();
         getInfoDelay();
 
-        /*if (mortgage.getIsAnnuity()) {
-            mortgage.setMonthlyPaymentAnnuity(calculations.calculateMonthlyPaymentAnnuity(mortgage));
-
-            mortgage.setMonthlyBalance(calculations.loanBalanceAnnuity(mortgage));
-
-            mortgage.setLoanPart(calculations.calculateLoanPartAnnuity(mortgage));
-
-            mortgage.setInterestPart(calculations.calculateInterestPartAnnuity(mortgage));
-
-            calculated = true;
-        } else if (mortgage.getIsLinear()) {
-            mortgage.setMonthlyPaymentLinear(calculations.calculateMonthlyPaymentLinear(mortgage));
-
-            mortgage.setMonthlyBalance(calculations.loanBalanceLinear(mortgage));
-
-            mortgage.setLoanPart(calculations.calculateLoanPartLinear(mortgage));
-
-            mortgage.setInterestPart(calculations.calculateInterestPartLinear(mortgage));
-
-            calculated = true;
-        }*/
-
         mortgage.setMonthlyPayment(calculations.calculateMonthlyPayment(mortgage));
         mortgage.setMonthlyBalance(calculations.loanBalance(mortgage));
         mortgage.setLoanPart(calculations.calculateLoanPart(mortgage));
         mortgage.setInterestPart(calculations.calculateInterestPart(mortgage));
 
-
         calculated = true;
-
     }
 
     public void export() throws IOException {
@@ -143,4 +125,39 @@ public class MainController {
         new ExcelData(mortgage);
     }
 
+    public void viewChart() {
+        if (!calculated)
+            calculate();
+
+        getInfoView();
+
+        // Show data in chart
+
+
+        // Create series for chart
+        XYChart.Series<Integer,Number> series1 = new XYChart.Series<>();
+        XYChart.Series<Integer,Number> series2 = new XYChart.Series<>();
+        XYChart.Series<Integer,Number> series3 = new XYChart.Series<>();
+
+        series1.getData().clear();
+        series2.getData().clear();
+        series3.getData().clear();
+
+        series1.setName("Total payment");
+        series2.setName("Interest payment");
+        series3.setName("Loan payment");
+
+        for(int i = mortgage.getViewFrom(); i < mortgage.getViewTo(); i++){
+            series1.getData().add(new XYChart.Data<>(i, Precision.round(mortgage.getMonthlyPayment()[i], 2)));
+            series2.getData().add(new XYChart.Data<>(i, Precision.round(((mortgage.getInterestPart()[i]*mortgage.getMonthlyPayment()[i])/100), 2)));
+            series3.getData().add(new XYChart.Data<>(i, Precision.round(((mortgage.getLoanPart()[i]*mortgage.getMonthlyPayment()[i])/100), 2)));
+
+            System.out.println(i+1 + " " + mortgage.getMonthlyPayment()[i] + " " + mortgage.getInterestPart()[i] + " " + mortgage.getLoanPart()[i]);
+        }
+        List<XYChart.Series<Integer, Number>> seriesList = Arrays.asList(series2, series1, series3);
+        ObservableList<XYChart.Series<Integer, Number>> observableSeriesList = FXCollections.observableList(seriesList);
+        chart.setData(observableSeriesList);
+
+
+    }
 }
